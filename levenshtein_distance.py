a704f95a39e717fa904490b6cd72a96aa1ee92e8
Louis Xu <louis.s.xu@gmail.com>
2021-03-16 01:59:50 +1100

    Edit comments for brevity.

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -1,6 +1,6 @@
 import sys
 
-def levenshtein_distance(string_a, string_b, count = None):
+def levenshtein_distance(string_a, string_b, count = 0):
     '''Returns the levenshtein distance between two strings
     Args:
         string_a (str): first string to be compared
@@ -10,28 +10,20 @@ def levenshtein_distance(string_a, string_b, count = None):
         result (int): the calculated levenshtein distance
     '''
 
-    # Initialise count so same function can be used for the recursion
-    if not count:
-        count = 0
-
-    # Base case
-    # One or more of the strings are empty. Return the total count
+    # Base case - one or more of the strings are empty. Return the total count
     if string_a == "" and string_b == "":
         return count
-
-    # Need to explicitly handle one string empty otherwise insert/delete recursion tries to slice empty strings
     if string_a == "":
         return count + len(string_b)
     if string_b == "":
         return count + len(string_a)
 
     # Recursive cases
-
     # Try to match letter for free
     if string_a[0] == string_b[0]:
         return match_letter(string_a, string_b, count)
 
-    # Otherwise, do spendy task
+    # Otherwise, do task with cost
     else:
         return min(
             change_letter(string_a, string_b, count),
@@ -40,30 +32,16 @@ def levenshtein_distance(string_a, string_b, count = None):
         )
 
 def match_letter(a, b, count):
-    # pig -> pat
-    # ig -> at
-    # Count does not increment. "Free letter"
-    return levenshtein_distance(a[1:], b[1:], count)
+    return levenshtein_distance(a[1:], b[1:], count) # Count does not increment. "Free letter"
 
 def change_letter(a, b, count):
-    # dog -> cat
-    # cog -> cat
-    # og -> at (includes the step done by match letter)
-    # Count increments since this is a costly action
-    return levenshtein_distance(a[1:], b[1:], count+1)
+    return levenshtein_distance(a[1:], b[1:], count + 1)
 
 def insert_letter(a, b, count):
-    # dog -> cat
-    # cdog -> cat
-    # dog -> at (including the step done by match letter)
-    # Count increments
-    return levenshtein_distance(a, b[1:], count+1)
+    return levenshtein_distance(a, b[1:], count + 1)
 
 def delete_letter(a, b, count):
-    # dog -> cat
-    # og -> cat
-    # Count increments
-    return levenshtein_distance(a[1:], b, count+1)
+    return levenshtein_distance(a[1:], b, count + 1)
 
 
 def main():
