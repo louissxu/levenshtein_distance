da74b4a3b52761579c9f0fce59531af5de388a61
Louis Xu <louis.s.xu@gmail.com>
2021-03-15 23:55:01 +1100

    Create unit tests.

---
+++ test_levenshtein_distance.py
@@ -0,0 +1,35 @@
+# Import unittest library and function being tested
+import unittest
+from levenshtein_distance import levenshtein_distance as ld
+
+class Tests(unittest.TestCase):
+    def test_1(self):
+        """Check both null strings"""
+        self.assertEqual(ld("", ""), 0)
+
+    def test_2(self):
+        """Check null first string"""
+        self.assertEqual(ld("", "a"), 0)
+
+    def test_3(self):
+        """Check null second string"""
+        self.assertEqual(ld("a", ""), 0)
+
+    def test_4(self):
+        """Check dog -> cat"""
+        self.assertEqual(ld("dog", "cat"), 3)
+
+    def test_5(self):
+        """Check cog -> cat"""
+        self.assertEqual(ld("cog", "cat"), 2)
+
+    def test_6(self):
+        """Check car -> racecar"""
+        self.assertEqual(ld("car", "racecar"), 4)
+
+    def test_7(self):
+        """Check race -> racecar"""
+        self.assertEqual(ld("race", "racecar"), 3)
+
+if __name__ == "__main__":
+    unittest.main()
