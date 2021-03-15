b436be6d3afc8bf334a6bc23fe23a9c4e741a066
Louis Xu <louis.s.xu@gmail.com>
2021-03-16 00:40:27 +1100

    First try at pseudocode.

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -12,6 +12,40 @@ def levenshtein_distance(string_a, string_b):
 
     raise NotImplementedError
 
+    
+    # Probable pseudocode
+
+    # base case
+    # if a and b are empty return count
+
+    # else    
+    # recursive case
+    # return minimum of:
+    # match_letter
+    # change letter
+    # insert_letter
+    # delete_letter
+
+
+    # match_letter(a, b, count):
+    #     dog -> dark
+    #     return og, ark, count
+
+    # change_letter(a, b, count):
+    #     dog -> cat
+    #     return og, at, count +=1
+
+    # insert_letter(a, b, count):
+    #     dog -> cat
+    #     cdog -> cat
+    #     return cdog, cat, count += 1 (and let subsequent match remove one) or simplify down and
+    #     return dog, at, count += 1
+
+    # delete_letter(a, b, count):
+    #     dog -> cat
+    #     og -> cat
+    #     return og, cat count += 1 
+
 
 def main():
     # Check command-line arguments
